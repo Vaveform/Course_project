360fef01766342dd6f2d206a8d4796a80acd1ab4
Vaveform <47713033+Vaveform@users.noreply.github.com>
2019-11-03 22:35:17 +0300

    Update Data_base.cpp
    First attempt to make class Database

--- Data_base/src/Data_base.cpp
+++ Data_base/src/Data_base.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <cmath>
 #include <exception>
+#include <iomanip>
 
 using namespace std;
 
@@ -38,9 +39,35 @@ class Database {
 private:
   map<Date, set<string>> pair;
 public:
-  void AddEvent(const Date& date, const string& event);
-  bool DeleteEvent(const Date& date, const string& event);
-  int  DeleteDate(const Date& date);
+  void AddEvent(const Date& date, const string& event)
+  {
+	  pair[date].insert(event);
+  };
+  bool DeleteEvent(const Date& date, const string& event)
+  {
+	  if(pair.count(date) > 0 && pair.at(date).count(event) > 0)
+	  {
+		  pair.at(date).erase(pair.at(date).find(event));
+		  return true;
+	  }
+	  else{
+		  return false;
+	  }
+  };
+  int  DeleteDate(const Date& date)
+  {
+	  auto finder = pair.find(date);
+	  int result = 0;
+	  if(finder != pair.end())
+	  {
+		  result = pair.at(date).size();
+		  pair.erase(finder);
+	  }
+	  else{
+		  throw runtime_error("Unknown signal 6");
+	  }
+	  return result;
+  };
 
   void Find(const Date& date) const
   {
@@ -54,7 +81,19 @@ public:
 	  }
   };
 
-  void Print() const;
+  void Print() const
+  {
+	  for(const auto& item: pair)
+	  {
+		  for(const auto& event: item.second)
+		  {
+			  cout << setfill('0') << setw(4) << item.first.GetYear() << "-"
+					  << setw(2) << item.first.GetMonth()
+					  << setw(2) << item.first.GetDay()
+					  << " " << event << endl;
+		  }
+	  }
+  };
 };
 
 int main()
